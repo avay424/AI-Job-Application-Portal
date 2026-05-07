3a837192aa9a21b646e10a30606582bb7d1eea7a
avay424 <mavay39@gmail.com>
2026-05-07 19:09:09 +0530

    changes nodemailer

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -16,13 +16,16 @@ dotenv.config();
 //   }
 // });
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  // service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
+  },
+    tls: {
+    rejectUnauthorized: false
   }
 });
 
